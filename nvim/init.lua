027e8b3a459be1bdf58cd71f3f88f4b4d81e9864
Zachary Alan Saylor <zchrysaylor@gmail.com>
2026-03-24 01:13:59 +0100

    move ghossty config and add ghostty dark theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -913,17 +913,25 @@ require('lazy').setup({
   },
 
   -- Colorscheme
+  -- {
+  --   'zenbones-theme/zenbones.nvim',
+  --   -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+  --   -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+  --   -- In Vim, compat mode is turned on as Lush only works in Neovim.
+  --   dependencies = 'rktjmp/lush.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.o.background = 'dark'
+  --     vim.cmd.colorscheme 'seoulbones'
+  --   end,
+  -- },
   {
-    'zenbones-theme/zenbones.nvim',
-    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-    -- In Vim, compat mode is turned on as Lush only works in Neovim.
-    dependencies = 'rktjmp/lush.nvim',
-    lazy = false,
+    'nkxxll/ghostty-default-style-dark.nvim',
+    name = 'ghostty-default-style-dark',
     priority = 1000,
     config = function()
-      vim.o.background = 'dark'
-      vim.cmd.colorscheme 'seoulbones'
+      vim.cmd.colorscheme 'ghostty-default-style-dark'
     end,
   },
 
